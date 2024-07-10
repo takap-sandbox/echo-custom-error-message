4792f225560459cd7364b88eb498f80f80a34c24
takapdayon <business.takap@gmail.com>
2024-07-11 00:06:24 +0900

    change: customHTTPErrorの追加

--- server.go
+++ server.go
@@ -5,9 +5,26 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+func customHTTPErrorHandler(err error, c echo.Context) {
+	he, ok := err.(*echo.HTTPError);
+	if !ok {
+		c.Echo().DefaultHTTPErrorHandler(err, c)
+		return
+	}
+	switch m := he.Message.(type) {
+	case []string:
+		err = c.JSON(he.Code, map[string]interface{}{"message": m})
+	default:
+		c.Echo().DefaultHTTPErrorHandler(err, c)
+	}
+}
+
 func main() {
 	e := echo.New()
+	e.HTTPErrorHandler = customHTTPErrorHandler
+
 	e.GET("/", func(c echo.Context) error {
+		return echo.NewHTTPError(http.StatusUnauthorized, []string{"エラーなのだ", "エラーなのだ2"})
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
